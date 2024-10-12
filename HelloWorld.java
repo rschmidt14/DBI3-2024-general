bab714a00ce196e8c5b2e6e67ae51a68d380cc44
Manta-Maka <massimo.bene@outlook.at>
2024-10-12 15:37:30 +0200

    Füge Datei HelloWorld.java mit interaktiven Modus hinzu

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,15 @@
-//import java.util.Scanner;
-class HelloWorld {
-  public static void main(String []args) {
-    System.out.println("Hello World!");
-  }
-};
+import java.util.Scanner;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        if (args.length > 0 && args[0].equals("i")) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("What's your name?");
+            String name = scanner.nextLine();
+            System.out.println("Hello " + name + "!");
+        } else {
+            System.out.println("Hello, World");
+        }
+    }
+}
+
