50bc9550b5c1e91e9fbcec2cb64247cfecc6b707
Dizdarevic <denis@dizdarevic94@hotmail.com>
2024-10-13 14:43:29 +0200

    Interactive Hello World

--- JavaDB/DB_Smir_Project/src/App.java
+++ JavaDB/DB_Smir_Project/src/App.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+    Scanner myObj = new Scanner(System.in);
+    String userInput = myObj.nextLine();
+    if(userInput.equalsIgnoreCase("i")){
+        System.out.println("Whats your name?");
+        String userName = myObj.nextLine();
+        System.out.println("Hello"+" "+userName+"!!");
     }
+
+    myObj.close();
+}
 }
