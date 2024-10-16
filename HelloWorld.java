fdac08fa29f8a07772a9b76df9e3130c6d3375f3
Mario Hess <office.hess@gmail.com>
2024-10-16 15:11:56 +0200

    :sparkles: Add interactive mode

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,26 @@
-// import java.util.Scanner;
-//
+import java.util.Scanner;
+
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        boolean iFlag = false;
+
+        for (String arg : args) {
+            if (arg.equals("i")) {
+                iFlag = true;
+                break;
+            }
+        }
+
+        if (iFlag) {
+            Scanner myObj = new Scanner(System.in);
+            System.out.println("What's your name?");
+
+            String userName = myObj.nextLine();
+            System.out.println("Hello " + userName + "!"); // Output user input
+
+            myObj.close();
+        } else {
+            System.out.println("Hello World!");
+        }
     };
 }
