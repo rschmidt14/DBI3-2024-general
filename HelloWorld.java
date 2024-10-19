de9577380df88cd9db55bab5a1af041653907d33
SanelK <sanel.kozlica90@htl-salzburg.ac.at>
2024-10-20 01:28:01 +0200

    Interaktiven Modus hinzugefügt

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,16 @@
-//import java.util.Scanner;
+import java.util.Scanner;
 
 class HelloWorld {
-	public static void main(String[] args) {
-        System.out.println("Hello World!");
-        }
- }
+    public static void main(String[] args) {
+        if (args.length > 0 && args[0].equals("i")) {
+            Scanner myObject = new Scanner(System.in);
+            System.out.println("What's your name?");
+
+            String name = myObject.nextLine();
+            System.out.println("Hello " + name +"!");
+
+        } else {
+            System.out.println("False argument typed");
+            }
+    }
+}
