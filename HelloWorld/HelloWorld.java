e1edc36ad3bf261fb7a901dab458e6308f255173
ikonoah <iko@gmx.net>
2024-10-21 16:19:55 +0200

    HelloWorld as java sript

--- HelloWorld/HelloWorld.java
+++ HelloWorld/HelloWorld.java
@@ -1,6 +1,17 @@
-//import java.util.Scanner;
-class HelloWorld {
-public static void main(String []args) {
-System.out.println("Hello World!");
+import java.util.Scanner;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        if (args.length > 0 && args[0].equals("i")) {
+            // Interaktiver Modus
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("What's your name?");
+            String name = scanner.nextLine();
+            System.out.println("Hello " + name + "!");
+            scanner.close();  // Schließen des Scanners
+        } else {
+            // Standardmodus
+            System.out.println("Hello World!");
+        }
+    }
 }
-};
