4161e81374a451ee8b6a380f7b834f58f97a8295
lsomoza1995 <luismckaysomozarojas@gmail.com>
2024-10-22 20:35:55 +0200

    java add

---
+++ helloWorld.java
@@ -0,0 +1,21 @@
+
+// import java.util.scanner;
+
+class HelloWorld{
+
+
+    public static void main(String [] args) {
+
+        system.out.println("Hello World");
+    }
+};
+
+class Main {
+    public static void main(String[] args) {
+      Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+      System.out.println("wossogga");
+  
+      String userName = myObj.nextLine();  // Read user input
+      System.out.println("Username is: " + userName);  // Output user input
+    }
+  }
