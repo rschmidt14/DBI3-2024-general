fb1c877e8dbd3912f8217cde8fa01c12688d6e28
Manta-Maka <massimo.bene@outlook.at>
2024-11-02 14:42:19 +0100

    Hash-calculator hinzufügen

---
+++ Hash-Calculcator/helloWorld/src/main/java/org/htl/kahr/HelloWorld.java
@@ -0,0 +1,11 @@
+package org.htl.kahr;
+
+import java.util.Scanner;
+/**
+ * Hello world!
+ */
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ Hash-Calculcator/helloWorld/src/test/java/org/htl/kahr/HelloWorldTest.java
@@ -0,0 +1,19 @@
+package org.htl.kahr;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class HelloWorldTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
