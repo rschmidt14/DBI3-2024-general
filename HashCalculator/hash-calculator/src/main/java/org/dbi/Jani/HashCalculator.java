0a4b342a9af8411eb7c569381d426238784b79b8
E0429C59DF <127661585+E0429C59DF@users.noreply.github.com>
2024-11-03 22:28:52 +0100

    Add files via upload

---
+++ HashCalculator/hash-calculator/src/main/java/org/dbi/Jani/HashCalculator.java
@@ -0,0 +1,10 @@
+package org.dbi.Jani;
+
+/**
+ * Hello world!
+ */
+public class HashCalculator {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
