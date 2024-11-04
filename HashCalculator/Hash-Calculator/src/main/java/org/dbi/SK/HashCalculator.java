0d2755e91fe5a7cb9cb36808697e5b1d3b741774
SanelK <sanel.kozlica90@htl-salzburg.ac.at>
2024-11-04 20:15:42 +0100

    Hash-Calculator Projekt hinzufügen

---
+++ HashCalculator/Hash-Calculator/src/main/java/org/dbi/SK/HashCalculator.java
@@ -0,0 +1,10 @@
+package org.dbi.SK;
+
+/**
+ * Hello world!
+ */
+public class HashCalculator {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ HashCalculator/Hash-Calculator/src/test/java/org/dbi/SK/AppTest.java
@@ -0,0 +1,19 @@
+package org.dbi.SK;
+
+/**
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+
+/*
+public class AppTest {
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+} */
