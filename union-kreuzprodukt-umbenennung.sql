3b31c8a0c00d560e25a0cf51a0cce52af6282e36
rschmidt14 <rainer@sciserver.at>
2024-11-19 21:36:36 +0100

    added examples

--- union-kreuzprodukt-umbenennung.sql
+++ union-kreuzprodukt-umbenennung.sql
@@ -48,9 +48,9 @@ create table s (
 
 select * from r,s where a = 1;
 
-drop table if exists lv;
-drop table if exists s;
-drop table if exists l;
+drop table if exists lv cascade;
+drop table if exists s cascade;
+drop table if exists l cascade;
 
 create table LV (
   id int,
@@ -70,7 +70,8 @@ create table S (
   sid int,
   sname text,
   alter int,
-  pendler bool
+  pendler bool,
+  kv int
 );
 
 truncate l;
@@ -86,16 +87,57 @@ insert into lv values
 (2, 'E', 6, 2022),
 (3, 'D', 2, 2022);
 
+--,
+insert into lv values(3, 'D', 6, 2023);
+
+
 insert into s values
-(10, 'Susi', 15, false),
-(20, 'Sepp', 16, false),
-(30, 'Max', 16, true);
+(10, 'Susi', 15, false, 1),
+(20, 'Sepp', 16, false, 3),
+(30, 'Max', 16, true, 3);
+
+-- vorgänger 
+insert into v values
+('Max', 'Erich'),
+('Gabi', 'Fritz'),
+('Erich', 'Moritz'),
+('Fritz', 'Hansi')
+;
 
 select * from l;
 select * from lv;
 select * from l, lv;
 select * from l, lv where lid = id;
 select lname, fach, stunden, jahr from l, lv where lid = id;
+-- die namen aller englischlehrer
+select lname from l, lv where lid = id and fach = 'E';
+-- wie viele stunden hat Max 2022 unterrichtet
+select stunden from l, lv where lid = id and lname = 'Max' and jahr = 2022;
+-- wie viele stunden hat Max 2022 unterrichtet
+select sum(stunden) from l, lv where lid = id and lname = 'Max' and jahr = 2022;
+-- wie viele Englischstunden hat Max 2022 unterrichtet
+select sum(stunden) from l, lv where lid = id and lname = 'Max' and jahr = 2022 and fach = 'E';
+-- welchen klassenvorstand (name) hat sepp
+select * from s;
+select * from l;
+select lname from s,l where kv = lid and sname = 'Sepp';
+-- die namen aller lehrer und fächer
+select lname as name from l
+union
+select fach as name from lv;
+
+-- der kv welcher schüler ist 'Gabi'
+-- alle schülernamen, alter, und kvnamen
+-- die namen aller pender (schüler und lehrer)
+-- alle lehrer die ein fach unterrichten (mit unterabfrage, mit kreuzprodukt)
+select * from lv;
+select distinct id from lv;
+select * from l;
+select * from l where lid in (select id from lv);
+select lname from l where lid in (select id from lv);
+-- alle lehrer die kein fach unterrichten
+-- alle fächer mit namen der unterrichtenden lehrer
+-- alle lehrer die kv sind
 
 -- name, lalter, salter bei namensgleichheit
 select sname, s.alter, l.alter from s, l where sname = lname;
