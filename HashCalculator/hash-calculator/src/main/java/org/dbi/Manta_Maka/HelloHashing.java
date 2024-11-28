10e56cad879d19ecff9f670cac9d9475b7a5f3e2
Manta-Maka <massimo.bene@outlook.at>
2024-11-28 20:40:18 +0100

    Hashgenerator fertiggestellt

--- HashCalculator/hash-calculator/src/main/java/org/dbi/Manta_Maka/HelloHashing.java
+++ HashCalculator/hash-calculator/src/main/java/org/dbi/Manta_Maka/HelloHashing.java
@@ -9,7 +9,7 @@ public class HelloHashing {
 
         HashGenerator hashGenerator = null;
         hashGenerator = new GuavaHashGenerator();
-        /*if(args != null && args.length == 1 && args[0].equalsIgnoreCase("Guava")) {
+        if(args != null && args.length == 1 && args[0].equalsIgnoreCase("Guava")) {
             hashGenerator = new GuavaHashGenerator();
         } else if(args != null && args.length == 1 && args[0].equalsIgnoreCase("Commons")) {
             hashGenerator = new CommonsHashGenerator();
@@ -17,7 +17,7 @@ public class HelloHashing {
             System.out.println("Usage: HelloHashing [Guava | Commons]");
             return;
         }
-    */
+
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter username: ");
         String userName = scanner.nextLine();
