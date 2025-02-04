c76f725e20b8bf64fcab4593de4629be54e4cf56
rschmidt14 <rainer@sciserver.at>
2025-02-04 20:30:24 +0100

    added lehrer db 1 zu n

--- outer-join.sql
+++ outer-join.sql
@@ -59,3 +59,51 @@ select a, b, c, d, x, y from r natural join s;
 select a, b, c, d, x, y from r natural left join s;
 select a, b, c, d, x, y from r natural right join s;
 select a, b, c, d, x, y from r natural full join s;
+
+-- neue 2024er Version der Lehrer-DB (1:N)
+-- zu eins beziehungen
+drop table if exists lv cascade;
+drop table if exists s cascade;
+drop table if exists l cascade;
+
+create table L (
+  id int primary key,
+  name text,
+  ALTER int,
+  pendler bool  
+);
+
+create table LV (
+  id int primary key,
+  fach char,
+  stunden int,
+  jahr int,
+  lid int references L
+);
+
+create table S (
+  id int primary key,
+  name text,
+  alter int,
+  pendler bool,
+  kv int references l
+);
+
+
+insert into l values
+(1, 'Max', 25, true),
+(2, 'Fritz', 31, false),
+(3, 'Gabi', 31, true),
+(4, 'Moritz', 33, false);
+
+insert into lv values 
+(1, 'D', 4, 2022, 1),
+(2, 'E', 4, 2022, 1),
+(3, 'E', 6, 2022, 2),
+(4, 'D', 2, 2022, 3),
+(5, 'D', 6, 2023, 3);
+
+insert into s values
+(10, 'Susi', 15, false, 1),
+(20, 'Sepp', 16, false, 3),
+(30, 'Max', 16, true, 3);
