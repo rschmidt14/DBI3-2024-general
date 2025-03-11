78e1e9d10a17d9790f77c8f47a9669c623e6b544
Mario Hess <office.hess@gmail.com>
2025-03-11 12:53:48 +0100

    Prepared statements

--- src/main/java/org/dbi/hess/UserService.java
+++ src/main/java/org/dbi/hess/UserService.java
@@ -5,24 +5,26 @@ import java.sql.*;
 public class UserService {
 
     public void saveUser(User user) throws SQLException {
-        String sql = "INSERT INTO users (username, password_hash) VALUES ('" + user.getUsername() + "', '" + user.getPasswordHash() + "')";
+        String sql = "INSERT INTO users (username, password_hash) VALUES (?, ?)";
         try (Connection conn = ConnectionFactory.getConnection();
-            Statement stmt = conn.createStatement()) {
-            stmt.executeUpdate(sql);
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1, user.getUsername());
+            pstmt.setString(2, user.getPasswordHash());
+            pstmt.executeUpdate();
         }
     }
 
     public User getUserByUsername(String username) throws SQLException {
-        String sql = "SELECT * FROM users WHERE username = '" + username + "'";
+        String sql = "SELECT * FROM users WHERE username = ?";
         try (Connection conn = ConnectionFactory.getConnection();
-             Statement stmt = conn.createStatement();
-             ResultSet rs = stmt.executeQuery(sql)) {
-            if (rs.next()) {
-                return new User(rs.getString("username"), null, rs.getString("password_hash"));
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1, username);
+            try (ResultSet rs = pstmt.executeQuery()) {
+                if (rs.next()) {
+                    return new User(rs.getString("username"), null, rs.getString("password_hash"));
+                }
             }
         }
         return null;
     }
-
-
 }
